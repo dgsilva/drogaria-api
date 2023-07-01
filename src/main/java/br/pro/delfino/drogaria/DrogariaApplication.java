bcc89c0391a1e7e04f29de515369b1aaea8b3b83
Diego Bizerra da Silva <diegorj93@gmail.com>
2023-07-01 17:20:14 -0300

    Implementado prometheus e grafana

--- src/main/java/br/pro/delfino/drogaria/DrogariaApplication.java
+++ src/main/java/br/pro/delfino/drogaria/DrogariaApplication.java
@@ -1,8 +1,12 @@
 package br.pro.delfino.drogaria;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,5 +16,13 @@ public class DrogariaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DrogariaApplication.class, args);
 	}
+	
+	
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+	    @Value("${spring.application.name}") String applicationName) {
+	    return (registry) -> registry.config().commonTags("application", applicationName);
+	}
+
 
 }
--- src/main/java/br/pro/delfino/drogaria/config/JwtConfiguration.java
+++ src/main/java/br/pro/delfino/drogaria/config/JwtConfiguration.java
@@ -31,7 +31,7 @@ public class JwtConfiguration extends WebSecurityConfigurerAdapter {
 	private static final String[] SWAGGER = {
 			"/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
 			"/configuration/security", "/swagger-ui.html", "/webjars/**",
-			"/v3/api-docs/**", "/swagger-ui/**","/actuator/health**"
+			"/v3/api-docs/**", "/swagger-ui/**","/actuator/health**","/actuator/prometheus**"
 	};
 
 	@Override
